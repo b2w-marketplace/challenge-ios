95993622fa784ab3755b18d3f8af2e7b26fd9929
Rodrigo Marangoni <romarangoni@gmail.com>
2018-01-29 21:20:49 -0200

    Addining Cocoapods, Alamofire, Swiftfy + banners fetch

--- teste/teste/BannerViewController.swift
+++ teste/teste/BannerViewController.swift
@@ -7,23 +7,52 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
+import SwiftyJSON
 
 class BannerViewController: UIViewController {
 
     @IBOutlet weak var bannerImage: UIImageView!
     @IBOutlet weak var pageControl: UIPageControl!
+    private var banners: Array<JSON>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.pageControl.numberOfPages = 5;
-
+        self.getBanners()
+        
         // Do any additional setup after loading the view.
     }
 
     @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
         self.pageControl.currentPage-=1;
+        self.setupBanner()
     }
     @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer) {
         self.pageControl.currentPage+=1;
+        self.setupBanner()
+    }
+    
+    func getBanners() {
+        Alamofire.request(APPURL.GetBanners, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let jsonData = JSON(value)
+                self.banners = jsonData["data"].arrayValue
+                self.pageControl.numberOfPages = self.banners.count
+                self.setupBanner()
+                
+                print("JSON: \(self.banners)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func setupBanner() {
+        if let urlImagem = URL(string: self.banners[self.pageControl.currentPage]["urlImagem"].stringValue) {
+            self.bannerImage.af_setImage(withURL: urlImagem)
+        }
+        
     }
 }
---
+++ teste/teste/URLConstants.swift
@@ -0,0 +1,27 @@
+//
+//  URLConstants.swift
+//  teste
+//
+//  Created by Rodrigo Marangoni on 29/01/18.
+//  Copyright © 2018 romarangoni. All rights reserved.
+//
+
+import Foundation
+
+struct APPURL {
+    
+    private  static let BaseURL = "https://alodjinha.herokuapp.com"
+    
+    static var GetBanners: String {
+        return BaseURL  + "/banner"
+    }
+    static var GetCategories: String {
+        return BaseURL + "/categoria"
+    }
+    static var GetProduto: String {
+        return BaseURL + "/produto"
+    }
+    static var GetMaisVendidos: String {
+        return BaseURL + "/produto/maisvendidos"
+    }
+}
