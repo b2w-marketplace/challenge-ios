01f87ef3f5f90cc2acf32efa751f855077b284eb
Daniel Novio <daniel.novio@buscapecompany.com>
2018-03-19 11:01:54 -0300

    Initial Commit

---
+++ Lodjinha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lodjinha
+//
+//  Created by Daniel Novio on 19/03/18.
+//  Copyright © 2018 Daniel Novio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LodjinhaTests/LodjinhaTests.swift
@@ -0,0 +1,36 @@
+//
+//  LodjinhaTests.swift
+//  LodjinhaTests
+//
+//  Created by Daniel Novio on 19/03/18.
+//  Copyright © 2018 Daniel Novio. All rights reserved.
+//
+
+import XCTest
+@testable import Lodjinha
+
+class LodjinhaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
