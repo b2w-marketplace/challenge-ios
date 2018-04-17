3e5e83e1281074b0c1d7bfe2b3f64d484c44027b
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2018-04-17 18:38:33 -0300

    Criado o projeto Lodginha e incluidas imagens nos Assets

---
+++ Lodginha/Lodginha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lodginha
+//
+//  Created by Ricardo Bailoni on 17/04/2018.
+//  Copyright © 2018 Ricardo Bailoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
