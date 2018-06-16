53ce70dd12dee48e84f5005f9d4b405f1dc3d0ed
Unknown <lazaro.lima.12@gmail.com>
2018-06-16 12:27:43 -0300

    Add gitIgnore

---
+++ desafiob2w/desafiob2wTests/desafiob2wTests.swift
@@ -0,0 +1,36 @@
+//
+//  desafiob2wTests.swift
+//  desafiob2wTests
+//
+//  Created by Lázaro Lima dos Santos on 14/06/18.
+//  Copyright © 2018 B2w. All rights reserved.
+//
+
+import XCTest
+@testable import desafiob2w
+
+class desafiob2wTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
