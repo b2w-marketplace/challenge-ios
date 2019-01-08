a7378258641ec2be36998f7834fc928f42c3a06a
Lucas Serrano <lucasgserrano@hotmail.com>
2019-01-08 21:03:08 -0200

    Configuração do projeto, adição da Fonte, ícone do App e LaunchScreen

---
+++ Lodjinha/LodjinhaTests/LodjinhaTests.swift
@@ -0,0 +1,34 @@
+//
+//  LodjinhaTests.swift
+//  LodjinhaTests
+//
+//  Created by Lucas Gonçalves Serrano on 08/01/19.
+//  Copyright © 2019 Lucas Gonçalves Serrano. All rights reserved.
+//
+
+import XCTest
+@testable import Lodjinha
+
+class LodjinhaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
