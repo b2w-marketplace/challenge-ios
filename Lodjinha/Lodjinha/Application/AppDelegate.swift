cfb84d2deaab21d56ded1f621aab374d03d329ac
Gilson Gil <gilson.gil@heartcare.life>
2019-02-21 23:45:02 -0300

    project created

---
+++ Lodjinha/Lodjinha/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Lodjinha
+//
+//  Created by Gilson Gil on 21/02/19.
+//  Copyright © 2019 Gilson Gil. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
