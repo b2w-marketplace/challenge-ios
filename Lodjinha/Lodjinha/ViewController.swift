187251d035de45aad7fae1f401575ffc98ae2cf5
gustavo <gustavo.az.ol@gmail.com>
2019-03-10 11:04:31 -0300

    Criacao do projeto
    Criando prjeto base.

---
+++ Lodjinha/Lodjinha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lodjinha
+//
+//  Created by Gustavo Azevedo de Oliveira on 10/03/19.
+//  Copyright © 2019 Gustavo Azevedo de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
