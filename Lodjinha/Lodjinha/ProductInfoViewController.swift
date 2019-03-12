25b4e470703416d00fa3c2414fc63345a623357a
gustavo <gustavo.az.ol@gmail.com>
2019-03-12 02:14:54 -0300

    Adicionando botao de reservar

--- Lodjinha/Lodjinha/ProductInfoViewController.swift
+++ Lodjinha/Lodjinha/ProductInfoViewController.swift
@@ -9,21 +9,35 @@
 import UIKit
 
 class ProductInfoViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var photoIv: UIImageView!
     @IBOutlet weak var nameLbl: UILabel!
     @IBOutlet weak var oldPriceLbl: UILabel!
     @IBOutlet weak var newPriceLbl: UILabel!
     @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var reserveBtn: UIButton!
     
     let presenter = ProductInfoPresenter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.setTexts()
+        self.adjustLayouts()
+    }
+    
+    private func setTexts() {
         self.photoIv.kf.setImage(with: self.presenter.photoUrl)
         self.nameLbl.text = self.presenter.productName
         self.oldPriceLbl.attributedText = self.presenter.oldPriceText
         self.newPriceLbl.text = self.presenter.newPriceString
         self.descriptionLbl.attributedText = self.presenter.productDescription
+    }
+    
+    private func adjustLayouts() {
+        self.reserveBtn.layer.cornerRadius = 10.0
+        self.scrollView.contentInset.bottom = 100.0
+    }
+    
+    @IBAction func reservePressed(_ sender: UIButton) {
     }
 }
