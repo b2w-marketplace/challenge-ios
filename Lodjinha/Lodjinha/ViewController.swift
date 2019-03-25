f570deb8bab7145b9883cd981ac0c647eeff8191
dougdss <douglas.sstt@gmail.com>
2019-03-24 23:29:28 -0300

    project created and gitignore added

---
+++ Lodjinha/Lodjinha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lodjinha
+//
+//  Created by Douglas da Silva Santos on 24/03/19.
+//  Copyright © 2019 Douglas da Silva Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
