8e20c7f1720c086d71eda0f676cc59b1b36fdbcd
dougdss <douglas.sstt@gmail.com>
2019-03-28 02:42:45 -0300

    added topSellingProducts

---
+++ Lodjinha/Lodjinha/Scenes/Home/Components/TopSellingProductTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TopSellingProductTableViewCell.swift
+//  Lodjinha
+//
+//  Created by Douglas da Silva Santos on 28/03/19.
+//  Copyright © 2019 Douglas da Silva Santos. All rights reserved.
+//
+
+import UIKit
+
+class TopSellingProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
