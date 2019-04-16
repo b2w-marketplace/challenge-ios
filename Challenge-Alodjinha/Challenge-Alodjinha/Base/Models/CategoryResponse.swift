1ae537bcea50ea360fa435670c4e8aae44f676e4
Luis Ferreira <luistf.mobile@gmail.com>
2019-04-16 18:39:06 -0300

    create Category Models

---
+++ Challenge-Alodjinha/Challenge-Alodjinha/Base/Models/CategoryResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryResponse.swift
+//  Challenge-Alodjinha
+//
+//  Created by Luis Ferreira on 16/04/19.
+//  Copyright © 2019 Luis Ferreira. All rights reserved.
+//
+
+import Foundation
+
+struct CategoryResponse: Codable {
+    var data: [Category]
+}
