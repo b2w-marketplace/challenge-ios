42c3d0b38e324898cd24c1494dda866f9349be0e
Luis Ferreira <luistf.mobile@gmail.com>
2019-04-16 14:53:44 -0300

    create HomeScreen and HomeViewModel

---
+++ Challenge-Alodjinha/Challenge-Alodjinha/Base/Modules/Home/HomeView.swift
@@ -0,0 +1,17 @@
+//
+//  HomeView.swift
+//  Challenge-Alodjinha
+//
+//  Created by Luis Ferreira on 16/04/19.
+//  Copyright © 2019 Luis Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Challenge-Alodjinha/Challenge-Alodjinha/Base/Modules/Home/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  Challenge-Alodjinha
+//
+//  Created by Luis Ferreira on 16/04/19.
+//  Copyright © 2019 Luis Ferreira. All rights reserved.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+}
