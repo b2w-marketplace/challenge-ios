ff39541d523e52df6cf509e447c69c9dd5ea164e
Luis Ferreira <luistf.mobile@gmail.com>
2019-09-05 21:52:05 -0300

    create Coordinatable, NavigationCoordinatable and AppCoordinator

---
+++ ALodjinha/ALodjinha/Sources/Application/Coordinator/AppCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  AppCoordinator.swift
+//  ALodjinha
+//
+//  Created by Luis Ferreira on 05/09/19.
+//  Copyright © 2019 Luis Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator: Coordinatable {
+    
+    private unowned var window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+}
+
+extension AppCoordinator {
+    func start() {
+    }
+}
---
+++ ALodjinha/ALodjinha/Sources/Application/Coordinator/Protocols/Coordinatable.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinatable.swift
+//  ALodjinha
+//
+//  Created by Luis Ferreira on 05/09/19.
+//  Copyright © 2019 Luis Ferreira. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinatable: class {
+    func start()
+}
