90f58f0914969146c9820112ee9e65a79b2be656
Luis Ferreira <luistf.mobile@gmail.com>
2019-09-05 23:58:41 -0300

    create MainCoordinator

--- ALodjinha/ALodjinha/Sources/Scenes/MainCoordinator.swift
+++ ALodjinha/ALodjinha/Sources/Scenes/MainCoordinator.swift
@@ -8,3 +8,19 @@
 
 import UIKit
 
+class MainCoordinator: NavigationCoordinatable {
+    
+    private lazy var homeView: HomeView = {
+        let viewController = HomeView(nibName: HomeString.HomeView, bundle: nil)
+        return viewController
+    }()
+    
+    lazy var navigationController: UINavigationController = {
+        let navigation = UINavigationController(rootViewController: self.homeView)
+        return navigation
+    }()
+    
+    var rootViewController: UIViewController {
+        return navigationController
+    }
+}
